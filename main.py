fb669998ce592f7ecce157a961c193cb59543b85
Aditya <mailingadityavs@gmail.com>
2020-01-08 03:55:24 +0530

    Base Project Completed. Conditions Added

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask , render_template , redirect 
+from flask import Flask , render_template , redirect , url_for , request, send_file
+import ytDownloader
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def home():
-    # return("Hello World")
-    return render_template("index.html")
+    if(request.method=="POST"):
+        link = request.form["link"]
+        downloadLink = ytDownloader.getFile(link)
+        return render_template("index.html",downloadLink = downloadLink)
+
+    return render_template("index.html",downloadLink = '/')
+
 
 if __name__ == "__main__" :
-    app.run()
+    app.run(debug=True)
--- ytDownloader.py
+++ ytDownloader.py
@@ -2,36 +2,43 @@ import requests
 from sys import argv
 import re
 
-url = "https://www.youtube.com/watch?v=sWrqyQWfhrs"
 
 # if argv == 2 :
 #     url = argv[1]
 # else :
 #     url = input("Enter Yotube URL : ")
 
-if(not "http" in url):
-    url = "https://" + url
+# if(not "http" in url):
+#     url = "https://" + url
 
-videoId = url.split("watch?v=")[1]
+# videoId = url.split("watch?v=")[1]
 
-r = requests.get(url)
+def getFile(url):
 
-# with open("fileHTML.txt","wb") as file :
-#     file.write(r.text.encode('utf8'))
+    r = requests.get(url)
+    title = re.findall(r'\\\"title\\\":\\\"(.+?)\\\"',r.text)[0]
+    title = title.split()
+    title = "+".join(title)
+    print(title)
+    links = re.findall(r'(https:.+?)\"', r.text)
+    linksFinal = []
+    for link in links:
+        if("googlevideo.com" in link):
+            link = link + '&contentlength=178814912'
+            link = link + '&title=' + title
+            link = link.replace("\\u0026", "&")
+            link = link.replace("\\&", "&")
+            link = link.replace("\\/", "/")
+            linksFinal.append(link)
 
-links = re.findall(r'(https:.+?)\"', r.text)
-linksFinal = []
-for link in links:
-    if("googlevideo.com" in link):
-        link = link + '&contentlength=178814912'
-        link = link.replace("\\u0026", "&")
-        link = link.replace("\\&", "&")
-        link = link.replace("\\/", "/")
-        linksFinal.append(link)
+    videoLink = linksFinal[3]
+    return videoLink
+
+
+
+
 
 # videoLink = linksFinal.pop()
-videoLink = linksFinal[3]
-
 # videoLink = videoLink + '&contentlength=178814912'
 # # videoLink = videoLink + '&' + videoId
 # # videoLink = videoLink + '&title='
@@ -40,7 +47,7 @@ videoLink = linksFinal[3]
 # videoLink = videoLink.replace("\\&", "&")
 # videoLink = videoLink.replace("\\/", "/")
 
-print(videoLink)
+# print(videoLink)
 # pyperclip.copy(videoLink)
 
 # wget.download(videoLink)
